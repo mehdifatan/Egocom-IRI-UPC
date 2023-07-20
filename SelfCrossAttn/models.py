eeb8caaf9382e6f8981252e71144de0f0b678551
Mehdi Fatan Serj <38161828+mehdifatan@users.noreply.github.com>
2023-07-20 23:57:29 +0100

    Add files via upload

---
+++ SelfCrossAttn/models.py
@@ -0,0 +1,182 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class TVAModel_Self(nn.Module): # trimodal self-attn model  
+    def __init__(self, params):
+        super(TVAModel_Self, self).__init__()
+        params.txt_rnnsize=25
+        params.rnnsize=50
+        params.vid_rnnsize=50
+        params.txt_rnnsize=50
+        params.aud_rnnsize=50
+        
+        #print(params.rnndir)
+        #print(params.txt_rnnnum)
+        
+
+        rnn = nn.LSTM if params.rnntype == "lstm" else nn.GRU
+        self.text_encoder = rnn(input_size=params.txt_dim, hidden_size=50,
+                                num_layers=params.txt_rnnnum, dropout=params.txt_rnndp, bidirectional=params.rnndir,
+                                batch_first=True)
+        
+        #self.video_conv = nn.Conv1d(in_channels=32, out_channels=25, kernel_size=1) # 825 params
+        self.video_conv = nn.Conv1d(in_channels=10, out_channels=10, kernel_size=1) # 825 params
+
+        self.video_encoder = rnn(input_size=params.vid_dim, hidden_size=params.vid_rnnsize,
+                                 num_layers=params.vid_rnnnum, dropout=params.vid_rnndp, bidirectional=params.rnndir,
+                                 batch_first=True)
+        self.audio_conv = nn.Conv1d(in_channels=10, out_channels=10, kernel_size=1)
+        self.audio_encoder = rnn(input_size=params.aud_dim, hidden_size=params.aud_rnnsize,
+                                 num_layers=params.aud_rnnnum, dropout=params.aud_rnndp, bidirectional=params.rnndir,
+                                 batch_first=True)
+        if params.rnndir:
+            self.mha_t = nn.MultiheadAttention(embed_dim=params.rnnsize*2, num_heads=params.txt_nh,
+                                                 dropout=params.txt_dp, batch_first=True)
+            self.mha_v = nn.MultiheadAttention(embed_dim=params.rnnsize*2, num_heads=params.vid_nh,
+                                               dropout=params.vid_dp, batch_first=True)
+            self.mha_a = nn.MultiheadAttention(embed_dim=params.rnnsize*2, num_heads=params.aud_nh,
+                                               dropout=params.aud_dp, batch_first=True)
+            self.concat_linear = nn.Linear(in_features=200, out_features= 20)
+            self.classifier = nn.Linear(in_features= 20, out_features=params.output_dim)
+
+    def forward(self, x_txt, x_vid, x_mfcc, emb_dp=0.25):
+        #x_txt = torch.unsqueeze(x_txt, dim=0)
+        #x_vid = torch.unsqueeze(x_vid, dim=0)
+        #x_mfcc = torch.unsqueeze(x_mfcc, dim=0)
+        # text branch
+        x_txt = F.dropout(x_txt, p=emb_dp, training=self.training) 
+        #print(x_txt.size())
+        x_txt, h = self.text_encoder(x_txt)
+        #print(h.size())
+        #print(x_txt.size())
+        x_txt, _ = self.mha_t(x_txt,x_txt,x_txt) 
+        #print(x_txt.size())
+        x_txt2 = torch.mean(x_txt, dim=1)
+        
+        x_txt2 = x_txt
+
+        #print(x_txt.size())
+        # video branch
+        x_vid = self.video_conv(x_vid)
+        #print(x_vid.size())
+        x_vid, h = self.video_encoder(x_vid)
+        #print(x_vid.size())
+        x_vid, _ = self.mha_v(x_vid, x_vid, x_vid) 
+        #print(x_vid.size())
+        #x_vid2 = torch.mean(x_vid, dim=1)
+        
+        x_vid2 = x_vid
+        
+        #print(x_vid2.size())
+        # audio branch
+        x_mfcc = self.audio_conv(x_mfcc)
+        #print(x_mfcc.size())
+        x_mfcc, h = self.audio_encoder(x_mfcc)
+        #print(x_mfcc.size())
+        x_mfcc, _ = self.mha_a(x_mfcc, x_mfcc, x_mfcc)
+        #print(x_mfcc.size())
+        #x_mfcc2 = torch.mean(x_mfcc, dim=1)
+        
+        x_mfcc2 = x_mfcc
+
+        #print(x_mfcc2.size())
+        #print(x_vid2.size())
+        #print(x_mfcc2.size())
+        
+        #x_tva1 = torch.stack((x_txt2, x_vid2, x_mfcc2), dim=1)
+
+        x_tva1 = torch.stack((x_txt2, x_vid2, x_mfcc2), dim=2)
+        
+        #print(x_tva1.size())
+        
+        x_tva1_mean, x_tva1_std = torch.std_mean(x_tva1, dim=2)
+
+        
+        #print(x_tva1_mean.size())
+        #print(x_tva1_std.size())
+
+        x_tva = torch.cat((x_tva1_mean, x_tva1_std), dim=2) 
+        
+        #print("x_tva",x_tva.size())
+        x_tva = self.concat_linear(x_tva)
+        y = self.classifier(x_tva) # [32, 7]
+        return y, x_tva
+    
+    
+class TVAModel_Cross(nn.Module): # trimodal cross-attn model
+    def __init__(self, params):
+        super(TVAModel_Cross, self).__init__()
+        rnn = nn.LSTM if params.rnntype == "lstm" else nn.GRU
+        self.text_encoder = rnn(input_size=params.txt_dim, hidden_size=params.txt_rnnsize,
+                                num_layers=params.txt_rnnnum, dropout=params.txt_rnndp, bidirectional=params.rnndir,
+                                batch_first=True)
+        self.video_conv = nn.Conv1d(in_channels=25, out_channels=25, kernel_size=1) # 825 params
+        self.video_encoder = rnn(input_size=params.vid_dim, hidden_size=params.vid_rnnsize,
+                                 num_layers=params.vid_rnnnum, dropout=params.vid_rnndp, bidirectional=params.rnndir,
+                                 batch_first=True)
+        self.audio_conv = nn.Conv1d(in_channels=25, out_channels=25, kernel_size=1)
+        self.audio_encoder = rnn(input_size=params.aud_dim, hidden_size=params.aud_rnnsize,
+                                 num_layers=params.aud_rnnnum, dropout=params.aud_rnndp, bidirectional=params.rnndir,
+                                 batch_first=True)
+        if params.rnndir:
+            self.mha_v_t = nn.MultiheadAttention(embed_dim=2 * params.rnnsize, num_heads=params.vt_nh,
+                                               dropout=params.vt_dp, batch_first=True)
+            self.mha_a_t = nn.MultiheadAttention(embed_dim=2 * params.rnnsize, num_heads=params.at_nh,
+                                                 dropout=params.at_dp, batch_first=True)
+            self.mha_t_v = nn.MultiheadAttention(embed_dim=2 * params.rnnsize, num_heads=params.tv_nh,
+                                                 dropout=params.tv_dp, batch_first=True)
+            self.mha_a_v = nn.MultiheadAttention(embed_dim=2 * params.rnnsize, num_heads=params.av_nh,
+                                                 dropout=params.av_dp, batch_first=True)
+            self.mha_t_a = nn.MultiheadAttention(embed_dim=2 * params.rnnsize, num_heads=params.ta_nh,
+                                                 dropout=params.ta_dp, batch_first=True)
+            self.mha_v_a = nn.MultiheadAttention(embed_dim=2 * params.rnnsize, num_heads=params.va_nh,
+                                                 dropout=params.va_dp, batch_first=True)
+            self.concat_linear = nn.Linear(in_features=2 * 2 * params.rnnsize, out_features= params.rnnsize)
+            self.classifier = nn.Linear(in_features= params.rnnsize, out_features=params.output_dim)
+
+    def forward(self, x_txt, x_vid, x_mfcc, emb_dp=0.25):
+        # text branch
+        x_txt = F.dropout(x_txt, p=emb_dp, training=self.training) 
+        x_txt, h = self.text_encoder(x_txt) 
+        # video branch
+        x_vid = self.video_conv(x_vid)
+        x_vid, h = self.video_encoder(x_vid)
+        # audio branch
+        x_mfcc = self.audio_conv(x_mfcc)
+        x_mfcc, h = self.audio_encoder(x_mfcc)
+        ##### V,A -> T
+        # video to text
+        x_v2t, _ = self.mha_v_t(x_txt, x_vid, x_vid) 
+        x_v2t = torch.mean(x_v2t, dim=1)
+        # audio to text
+        x_a2t, _ = self.mha_a_t(x_txt, x_mfcc, x_mfcc)
+        x_a2t = torch.mean(x_a2t, dim=1)
+        # addition
+        ####### T,A -> V
+        # text to video
+        x_t2v, _ = self.mha_t_v(x_vid, x_txt, x_txt) 
+        x_t2v = torch.mean(x_t2v, dim=1)
+        # audio to video
+        x_a2v, _ = self.mha_a_v(x_vid, x_mfcc, x_mfcc)
+        x_a2v = torch.mean(x_a2v, dim=1)
+        # addition
+        ####### T,V -> A
+        # text to audio
+        x_t2a, _ = self.mha_t_a(x_mfcc, x_txt, x_txt)
+        x_t2a = torch.mean(x_t2a, dim=1)
+        # video to audio
+        x_v2a, _ = self.mha_v_a(x_mfcc, x_vid, x_vid)
+        x_v2a = torch.mean(x_v2a, dim=1)
+        # addition
+        #import pdb
+        #pdb.set_trace()
+        x_tva2 = torch.stack((x_a2t, x_v2t, x_t2v, x_a2v, x_t2a, x_v2a), dim=1) 
+        x_tva2_mean, x_tva2_std = torch.std_mean(x_tva2, dim=1)
+        x_tva2 = torch.cat((x_tva2_mean, x_tva2_std), dim=1) 
+        x_tva = x_tva2
+        x_tva = self.concat_linear(x_tva)
+        y = self.classifier(x_tva) # [32, 7]
+        return y, x_tva
+
---
+++ vocalist/README.md
@@ -0,0 +1,60 @@
+# VocaLiST: An Audio-Visual Synchronisation Model for Lips and Voices
+[[Project Page]](https://ipcv.github.io/VocaLiST/) [[Arxiv]](https://arxiv.org/abs/2204.02090) [[Weights]](https://drive.google.com/drive/folders/1-g4qHUNNcCZpmSqEflKMxPMvwnn9e88N?usp=sharing)
+
+Official repository for the paper VocaLiST: An Audio-Visual Synchronisation Model for Lips and Voices. 
+
+The paper has been accepted to Interspeech 2022.
+
+<div class="center" style="text-align: center">
+    <div class="center col-md-8" style="text-align: center">
+        <img src="Vocalist_Illustration.png"/>
+    </div>
+</div>
+
+### Datasets
+There are 2 datasets involved in this work: i) The AV speech dataset of LRS2, 
+and ii) the AV singing voice dataset of Acappella.
+The LRS2 dataset can be requested for download [here](https://www.robots.ox.ac.uk/~vgg/data/lip_reading/lrs2.html).
+The Acappella dataset can be downloaded by following the steps mentioned [here](https://ipcv.github.io/Acappella/acappella/).
+
+### Preprocessing
+All the models considered in this work operate on audios sampled at 16kHz 
+and videos with 25fps. The preprocessing steps are the same as in 
+https://github.com/Rudrabha/Wav2Lip/blob/master/preprocess.py. The objective 
+of the preprocessing step is to obtain the cropped RGB face frames of size 3x96x96 
+in the .jpg format and audios of 16kHz sampling rate for each of the video samples in respective datasets.
+
+### Training
+To train the VocaLiST model on LRS2 dataset, run the program train_vocalist_lrs2.py.
+To train on Acappella dataset, run the program train_vocalist_acappella.py.
+Please remember to change the paths to save the checkpoint and the dataset related paths accordingly.
+Note that, by default, models are trained to detect synchronization in a context of 
+5 frames, which corresponds to audio of 3200 elements. To change the 
+context length, it is also necessary to change the length of audio, accordingly.
+
+
+### Testing
+To test the models, first download the pretrained model weights from [here](https://drive.google.com/drive/u/0/folders/1-g4qHUNNcCZpmSqEflKMxPMvwnn9e88N).
+To test on LRS2, run the program test_lrs2.py. 
+To test on Acappella dataset, run the program test_acappella.py.
+If you wish to evaluate on Acappella dataset only for a particular context, 
+run the program test_acappella_specific.py. Please remember to change the paths to the 
+pretrained weights and the dataset related paths accordingly.
+
+### Acknowledgements
+
+The evaluation code is adapted from  Out of time: automated lip sync in the wild. 
+The training  code and the experiment configuration setup is borrowed or adapted from that of A Lip Sync Expert Is All You Need for Speech to Lip Generation In the Wild.
+The code for internal components of the transformer block is borrowed from
+that of the work Multimodal Transformer for Unaligned Multimodal Language Sequences.
+
+### License
+This project makes use of source code of other existing works. 
+Only the original source code from this repository is provided under CC-BY-NC license, 
+while the parts of the repository based on the code reused/adapted from elsewhere 
+are available under their respective license terms. 
+The code for evaluation setup is adapted from the source code of the paper 
+''Out of time: automated lip sync in the wild'' which is made available under https://github.com/joonson/syncnet_python/blob/master/LICENSE.md.
+The code for the transformer encoder and its internal components are made available under https://github.com/yaohungt/Multimodal-Transformer/blob/master/LICENSE.
+The code related to reading the experiment configuration (hparams) is adapted from the code of https://github.com/Rudrabha/Wav2Lip/
+which is under non-commercial license terms. 
